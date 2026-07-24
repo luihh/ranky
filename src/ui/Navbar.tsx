ab19575095063eaae57e96c0e63d0b19b888c0ab
Luihh <luihh@proton.me>
2026-07-23 23:22:09 -0500

    Update navbar

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,31 +1,41 @@
 import { Link } from '@tanstack/react-router'
 import { Settings } from 'lucide-react'
-import { SiDiscord } from '@icons-pack/react-simple-icons'
+
+function Buttons() {
+  return (
+    <>
+      <a href="https://ko-fi.com/O5B023OVI4" target="_blank">
+        <img
+          height={36}
+          style={{ border: 0, height: 36 }}
+          src="https://storage.ko-fi.com/cdn/kofi3.png?v=6"
+          alt="Buy Me a Coffee at ko-fi.com"
+        />
+      </a>
+
+      <Link to="/settings" draggable={false}>
+        <button aria-label="Settings">
+          <Settings size={18} />
+        </button>
+      </Link>
+    </>
+  )
+}
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-col gap-3 py-4 px-8 bg-surface md:flex-row md:items-center md:justify-between">
-      <div className="flex items-center justify-between w-full md:w-auto">
-        <Link to="/" className="text-2xl font-bold no-underline whitespace-nowrap">
+    <nav className="flex flex-col gap-3 py-4 px-8 bg-surface lg:grid lg:grid-cols-[auto_1fr_auto] lg:items-center">
+      <div className="flex items-center justify-between w-full lg:w-auto">
+        <Link to="/" className="lg:w-3xs text-2xl font-bold no-underline whitespace-nowrap">
           Ranky :)
         </Link>
 
-        <div className="flex flex-row gap-3">
-          <a href="https://discord.gg/g5gZymxEyR" className="md:hidden" draggable={false}>
-            <button aria-label="Discord server">
-              <SiDiscord size={18} />
-            </button>
-          </a>
-
-          <Link to="/settings" className="md:hidden" draggable={false}>
-            <button aria-label="Settings">
-              <Settings size={18} />
-            </button>
-          </Link>
+        <div className="flex flex-row gap-3 lg:hidden">
+          <Buttons />
         </div>
       </div>
 
-      <form action="/search" className="flex w-full max-w-lg mx-auto">
+      <form action="/search" className="flex w-full max-w-lg mx-auto lg:justify-self-center">
         <input
           type="text"
           name="q"
@@ -40,17 +50,9 @@ export default function Navbar() {
         </button>
       </form>
 
-      <a href="https://discord.gg/g5gZymxEyR" className="hidden md:block">
-        <button aria-label="Discord server">
-          <SiDiscord size={18} />
-        </button>
-      </a>
-
-      <Link to="/settings" className="hidden md:block">
-        <button aria-label="Settings">
-          <Settings size={18} />
-        </button>
-      </Link>
+      <div className="hidden lg:flex lg:items-center lg:gap-3 lg:justify-end lg:w-3xs">
+        <Buttons />
+      </div>
     </nav>
   )
 }
